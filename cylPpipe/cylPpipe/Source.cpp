8f550e04d924eb3a24e5efb967d691ab45c0e2bb
Madhu Kumar D S <madhudskumar@live.in>
2016-05-10 02:49:09 +0530

    init

--- cylPpipe/cylPpipe/Source.cpp
+++ cylPpipe/cylPpipe/Source.cpp
@@ -2,6 +2,42 @@
 #include<GL/glut.h>
 #include<math.h>
 
-void obj(float x, float y, int num) {
+void obj(float cx, float cy, float r, int num) {
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < num; i++)
+	{
+		float theta = 2.0 * 3.14 * i / num;
+		float x = r*cosf(theta);
+		float y = r*sinf(theta);
+		glVertex2f(x + cx, y + cy);
+	}
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.0, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, 0.0);
+	glEnd();
+}
+
+void disp() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glViewport(0, 0, 700, 700);
+	glTranslated(0, 0, 0);
+	for (int i = 0; i < 30; i++) {
+		glTranslated(.01, 0.01, 0.01);
+		obj(.4, -.7, .3, 30);
+	}
+	glFlush();
+	glLoadIdentity();
+}
+
+void main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(700, 700);
+	glutCreateWindow("cylinder");
+	glutDisplayFunc(disp);
+	glutMainLoop();
 
 }
